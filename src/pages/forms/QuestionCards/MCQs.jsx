cfd05947008c4310a977450712d2725d35e0c185
Semora <senalweerasekara@gmail.com>
2024-05-13 20:47:18 +0530

    changes

--- src/pages/forms/QuestionCards/MCQs.jsx
+++ src/pages/forms/QuestionCards/MCQs.jsx
@@ -1,66 +1,118 @@
-import React from 'react';
+import React from "react";
 import { useForm } from "react-hook-form";
-import { useState } from 'react';
+import { useState } from "react";
 
-const MCQs = ({ question, answers, regQuestion, nselectedAnswer, onAnswerSelect, setFormData, color, borderColor }) => {
-    const [selectedAnswer, setSelectedAnswer] = useState(null);
-    const [textInputValue, setTextInputValue] = useState('');
-    const { register } = useForm();
-    { console.log('test2', nselectedAnswer) }
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData(prevData => ({
-            ...prevData,
-            [name]: value
-        }));
-    };
+const MCQs = ({
+  question,
+  answers,
+  regQuestion,
+  nselectedAnswer,
+  onAnswerSelect,
+  setFormData,
+  color,
+  borderColor,
+}) => {
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [textInputValue, setTextInputValue] = useState("");
+  const [radioOn, setRadio] = useState(true);
+  const { register } = useForm();
+  {
+    console.log("test2", nselectedAnswer);
+  }
 
-    const handleRadioChange = (event) => {
-        const value = event.target.value;
-        setSelectedAnswer(value === selectedAnswer ? null : value);
-    };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // if (name == value) {
+    //   setFormData((prevData) => ({
+    //     ...prevData,
+    //     [name]: null,
+    //   }));
+    // }
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
-    const handleTextInputChange = (event) => {
-        setTextInputValue(event.target.value);
-    };
+  const handleRadioChange = (event) => {
+    const value = event.target.value;
+    setSelectedAnswer(value === selectedAnswer ? null : value);
+  };
 
-    // const handleClick = (answerText) => {
-    //     onAnswerSelect(answerText);
-    // };
+  const handleTextInputChange = (event) => {
+    setTextInputValue(event.target.value);
+  };
 
-    return (
-        <div>
-            <label className="block text-xl font-semibold mb-2">
-                {question}
-            </label>
-            {answers.map((answer, index) => (
-                // <div key={index} onClick={() => handleClick(answer.text)} className={`cursor-pointer p-2 ${selectedAnswer === answer.text ? 'bg-sky-500' : 'bg-white'} rounded-md border border-gray-300`}>
-                <div key={index}>
-                    <label className="flex items-center mb-2">
-                        <input
-                            type="radio"
-                            name="favoriteSibling"
-                            value={answer.emotion}
-                            {...register(regQuestion)}
-                            onChange={handleChange}
-                            className="appearance-none h-8 w-8 border border-sky-700 rounded-full checked:bg-sky-500 checked:border-transparent focus:outline-none"
-                        />
-                        <span className="ml-2 text-xl">{answer.text}</span>
-                    </label>
-                </div>
-            ))}
+  const turnOnText = (question) => {
+    console.log("turn on text");
+    setFormData((prevData) => ({
+        ...prevData,
+        [question]: null,
+      }));
+    console.log("test2", question)
+    setRadio(false);
+  };
+  const turnOnRadio = () => {
+    console.log("turn on radio");
+    setRadio(true);
+  };
 
-            <input
-                type="text"
-                className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-400"
-                placeholder="Why do you think so?"
-                value={nselectedAnswer}
+  return (
+    <div>
+      <label className="block text-xl font-semibold mb-2">{question}</label>
+      <div>
+        {answers.map((answer, index) => (
+          // <div key={index} onClick={() => handleClick(answer.text)} className={`cursor-pointer p-2 ${selectedAnswer === answer.text ? 'bg-sky-500' : 'bg-white'} rounded-md border border-gray-300`}>
+          <div onClick={turnOnRadio}  key={index}>
+            <label className={`flex items-center mb-2 flex items-center mb-2 ${!radioOn ? 'pointer-events-none' : ''}`}>
+              <input
+                type="radio"
+                name="favoriteSibling"
+                value={answer.emotion}
                 {...register(regQuestion)}
                 onChange={handleChange}
-            />
+                className={`appearance-none h-8 w-8 border  rounded-full  ${
+                  radioOn
+                    ? "border-sky-700 checked:bg-sky-500"
+                    : "border-gray-300 checked:border-gray-300" 
+                }  checked:border-transparent focus:outline-none`}
+                disabled={!radioOn}
+              />
+              <span
+                className={`ml-2 text-xl ${
+                  radioOn ? "text-black" : "text-gray-300"
+                }`}
+              >
+                {answer.text}
+              </span>
+            </label>
+          </div>
+        ))}
+      </div>
 
-        </div>
-    );
+      {!radioOn ? (
+        <input
+          type="text"
+          className={`w-full px-3 ${
+            !radioOn ? "text-black" : "text-red-300"
+          } py-2 border rounded-md focus:outline-none focus:border-blue-400`}
+          placeholder="Why do you think so?"
+          value={nselectedAnswer}
+          {...register(regQuestion)}
+          onChange={handleChange}
+          disabled={radioOn}
+        />
+      ) : (
+        <input
+          type="text"
+          value={''}
+          className="w-full px-3 py-2 text-gray-400 border rounded-md focus:outline-none focus:border-blue-400"
+          placeholder="Why do you think so?"
+          onClick={() => turnOnText(regQuestion)}
+        />
+      )}
+    </div>
+  );
 };
 
 export default MCQs;
